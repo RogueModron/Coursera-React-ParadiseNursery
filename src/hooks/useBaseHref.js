09f5b9429f05234f7ac2918c17799494d28af675
RogueModron <lions.forever@gmail.com>
2025-02-24 20:28:02 +0000

    Fix to useBaseHref.

--- src/hooks/useBaseHref.js
+++ src/hooks/useBaseHref.js
@@ -3,7 +3,7 @@ import { useMemo } from "react";
 export function useBaseHref() {
     const baseRef = useMemo(() => {
         const bases = document.getElementsByTagName('base');
-        if (bases.length === 0) {
+        if (bases.length === 1) {
             const href = bases[0].getAttribute('href');
             if (typeof href === 'string' && href.length > 0) {
                 return href;
